17e45f88f4c7cba56ba5117e54b6c09948f62e52
Joeseidon <cutinoj@mail.gvsu.edu>
2018-04-18 12:42:54 -0400

    hard link

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
 
-void main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
+	int mode;	
+	struct stat statData;
 	if(argc < 3){
 		perror("Not Enough Arguments");
 		exit(0);
@@ -16,12 +20,27 @@ void main(int argc, char *argv[]){
 	
 	if(argc == 3){
 		//create hard link
-		
+		mode = 0;
 	}else{
+		
 		//for 4 arguments, create symbolic link
-		if(!(strcmp(argc[1],"-s") == 0)){
+		mode = 1;
+		if(!(strcmp(argv[1],"-s") == 0)){
 			perror("For Four Arguments, Flag Must be -s");
 			exit(0);
 		}
 	}	
+
+	//attempt to read file information
+	if(stat(argv[mode+1], &statData) < 0){
+		perror("Unable to get Stat information");
+		exit(1);
+	}
+
+	if(link(argv[mode+1],argv[mode+2])<0){
+		perror("Enabled to create hard lin");
+		exit(0);
+	}
+	
+	return 0;
 }
