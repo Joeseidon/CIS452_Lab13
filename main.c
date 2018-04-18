907a8d1222da6a6d031fbc46c0c0e8e4108d23e2
Joeseidon <cutinoj@mail.gvsu.edu>
2018-04-18 12:19:45 -0400

    code structure

--- main.c
+++ main.c
@@ -1,3 +1,27 @@
-void main(int argc, char *argv[]){
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+void main(int argc, char *argv[]){
+	if(argc < 3){
+		perror("Not Enough Arguments");
+		exit(0);
+	}
+
+	if(argc > 4){
+		perror("Too Many Arguments");
+		exit(0);
+	}
+	
+	if(argc == 3){
+		//create hard link
+		
+	}else{
+		//for 4 arguments, create symbolic link
+		if(!(strcmp(argc[1],"-s") == 0)){
+			perror("For Four Arguments, Flag Must be -s");
+			exit(0);
+		}
+	}	
 }
